16fc3a9e046771a304ec23fcd448ffea37ddbf9f
Rojkov Maxim <maximro.box@gmail.com>
2022-01-28 00:03:11 +0300

    Add Layout, fix footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import "./footer.scss";
 import sponsors from "@/components/footer/footerSources";
+import SponsorLogo from "@/components/footer/sponsorLogo";
 
 const Footer: FC = () => {
   const dateNow = new Date().getFullYear();
@@ -11,26 +12,9 @@ const Footer: FC = () => {
         <a href="/">OpenMXGIC</a>.
       </p>
       <ul className="social-icons">
-        <li>
-          <a className="ubisoft" href={sponsors.ubisoft.url} target="_blank" rel="noopener noreferrer">
-            <img src={sponsors.ubisoft.logo} alt={sponsors.ubisoft.description} />
-          </a>
-        </li>
-        <li>
-          <a className="eaGames" href={sponsors.eaGames.url} target="_blank" rel="noopener noreferrer">
-            <img src={sponsors.eaGames.logo} alt={sponsors.eaGames.description} />
-          </a>
-        </li>
-        <li>
-          <a className="rockStar" href={sponsors.rockStar.url} target="_blank" rel="noopener noreferrer">
-            <img src={sponsors.rockStar.logo} alt={sponsors.rockStar.description} />
-          </a>
-        </li>
-        <li>
-          <a className="steam" href={sponsors.steam.url} target="_blank" rel="noopener noreferrer">
-            <img src={sponsors.steam.logo} alt={sponsors.steam.description} />
-          </a>
-        </li>
+        {sponsors.map((sponsor) => (
+          <SponsorLogo sponsor={sponsor} />
+        ))}
       </ul>
     </footer>
   );
--- src/components/footer/footerSources.ts
+++ src/components/footer/footerSources.ts
@@ -3,19 +3,18 @@ import steamLogo from "images/SteamLogo.png";
 import rockstarLogo from "images/RockStarLogo.png";
 import ubiLogo from "images/UbisoftLogo.png";
 
-interface ISponsors {
-  [key: string]: {
-    url: string;
-    logo: string;
-    description: string;
-  };
+export interface ISponsor {
+  name: string;
+  url: string;
+  logo: string;
+  description: string;
 }
 
-const sponsors: ISponsors = {
-  eaGames: { url: "https://www.ea.com/ru-ru", logo: cdProjLogo, description: "EA Games log" },
-  steam: { url: "https://store.steampowered.com/?l=russian", logo: steamLogo, description: "Steam logo" },
-  rockStar: { url: "https://www.rockstargames.com/ru/", logo: rockstarLogo, description: "RockStar logo" },
-  ubisoft: { url: "https://www.ubisoft.com/ru-ru/", logo: ubiLogo, description: "Ubisoft logo" },
-};
+const sponsors: ISponsor[] = [
+  { name: "ubisoft", url: "https://www.ubisoft.com/ru-ru/", logo: ubiLogo, description: "Ubisoft logo" },
+  { name: "eaGames", url: "https://www.ea.com/ru-ru", logo: cdProjLogo, description: "EA Games log" },
+  { name: "rockStar", url: "https://www.rockstargames.com/ru/", logo: rockstarLogo, description: "RockStar logo" },
+  { name: "steam", url: "https://store.steampowered.com/?l=russian", logo: steamLogo, description: "Steam logo" },
+];
 
 export default sponsors;

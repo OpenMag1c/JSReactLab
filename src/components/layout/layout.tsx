26225e53f6cdbf6c21112cd15c02a2a9d896929b
Rojkov Maxim <maximro.box@gmail.com>
2022-02-27 12:34:42 +0300

    Fix auth

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,18 +1,42 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 import Modals from "@/components/elements/modal/modals";
+import { getUser } from "@/api/api";
+import Spinner from "@/components/elements/spinner/spinner";
+import useActions from "@/hooks/useActions";
+import { RootState } from "@/store";
 
-const Layout: FC = () => (
-  <>
-    <Header />
-    <main>
-      <Outlet />
-    </main>
-    <Modals />
-    <Footer />
-  </>
-);
+const Layout: FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const { isAuth } = useSelector((state: RootState) => state.auth);
+  const { signIn } = useActions();
+
+  useEffect(() => {
+    const userId = localStorage.getItem("id");
+    if (userId && !isAuth) {
+      (async () => {
+        const user = await getUser({ id: userId });
+        if (user) {
+          signIn(user);
+        }
+        setLoading(false);
+      })();
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main>{loading ? <Spinner /> : <Outlet />}</main>
+      <Modals />
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
